1157de127125ea4dcd274a328e1863fa249dbcf2
cwinters8 <clwinters8@gmail.com>
2019-03-30 15:22:07 -0500

    having issues with post requests to /users

--- routes.js
+++ routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const auth = require('basic-auth');
 const bcryptjs = require('bcryptjs');
+const {check, validationResult} = require('express-validator/check');
+
 const User = require('./models').User;
 const Course = require('./models').Course;
 
@@ -11,7 +13,6 @@ const authentication = (req, res, next) => {
     User.findOne({emailAddress: credentials.name}).exec((err, user) => {
       try {
         const authenticated = bcryptjs.compareSync(credentials.pass, user.password);
-        console.log(authenticated);
         if (authenticated) {
           console.log(`Authentication successful for user ${user.emailAddress}`);
           req.user = user.emailAddress;
@@ -19,16 +20,19 @@ const authentication = (req, res, next) => {
         } else {
           err.message = `Authentication failure for user ${user.emailAddress}`;
           console.error(err.message);
+          res.status(401);
           next(err);
         }
       } catch(err) {
         err.message = 'Invalid credentials';
         console.error(err);
+        res.status(401);
         next(err);
       }
     });
   } else {
     err.message = 'Auth header not found';
+    res.status(401);
     next(err);
   }
 }
@@ -38,13 +42,32 @@ router.get('/users', authentication, (req, res, next) => {
   res.json({user: req.user});
 });
 
+// create a new user
+router.post('/users', [
+  check('emailAddress').isEmail(),
+  check('password').isLength({min: 5})
+], (req, res, next) => {
+  console.log(req.body);
+  // check for validation errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    res.status(422);
+    next(errors.array());
+  }
+});
+
 // error handler
 router.use((err, req, res, next) => {
   res.status = err.status || 500;
+  let errorObject;
+  if (err.message) {
+    errorObject = {message: err.message};
+  } else {
+    errorObject = err;
+  }
   res.json({
-    error: {
-      message: err.message
-    }
+    error: errorObject
   });
 });
 
